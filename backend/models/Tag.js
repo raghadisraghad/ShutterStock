c178cb86eb49922b95867372d9e3eed886834635
raghadisraghad <raghad.ch.4@gmail.com>
2024-11-24 22:36:27 +0100

    frontend

--- backend/models/Tag.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const tagSchema = new mongoose.Schema({
-    name: { type: String },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
-});
-
-export default mongoose.model('Tag', tagSchema);
